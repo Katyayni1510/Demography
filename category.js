369b57743bae5d1ee434ef5c448589c03d47d8e5
Katyayni1510 <75209574+Katyayni1510@users.noreply.github.com>
2025-02-19 16:08:40 +0530

    Add files via upload

---
+++ category.js
@@ -0,0 +1,140 @@
+
+function Technology_click() {
+  document.getElementById("Technology").style.opacity = "1";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>20</span> out of the top 100 campanies are from Technology Sector holding a market capitalization of <span style='color: #FD5550;'> 33.00%</span>";
+}
+
+function Discretionary_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "1";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>17</span> out of the top 100 campanies are from Consumer Discretionary Sector holding a market capitalization of <span style='color: #FD5550;'> 18.90%</span>";
+}
+
+function Financials_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "1";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>14</span> out of the top 100 campanies are from Financials Sector holding a market capitalization of <span style='color: #FD5550;'> 10.80%</span>";
+}
+
+function Health_Care_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "1";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>16</span> out of the top 100 campanies are from Health Care Sector holding a market capitalization of <span style='color: #FD5550;'> 10.50%</span>";
+}
+
+function Energy_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "1";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>5</span> out of the top 100 campanies are from Energy Sector holding a market capitalization of <span style='color: #FD5550;'> 8.50%</span>";
+}
+
+function Consumer_Staples_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "1";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>9</span> out of the top 100 campanies are from Consumer Staples Sector holding a market capitalization of <span style='color: #FD5550;'> 6.40%</span>";
+}
+
+function Industrials_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "1";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>9</span> out of the top 100 campanies are from Industrials Sector holding a market capitalization of <span style='color: #FD5550;'> 6.40%</span>";
+}
+
+function Telecommunications_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "1";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>7</span> out of the top 100 campanies are from Telecommunications Sector holding a market capitalization of <span style='color: #FD5550;'> 4.10%</span>";
+}
+
+function Basic_Materials_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "1";
+  document.getElementById("Utilities").style.opacity = "0.2";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>2</span> out of the top 100 campanies are from Basic Materials Sector holding a market capitalization of <span style='color: #FD5550;'> 1.00%</span>";
+}
+
+function Utilities_click() {
+  document.getElementById("Technology").style.opacity = "0.2";
+  document.getElementById("Consumer_Discretionary").style.opacity = "0.2";
+  document.getElementById("Financials").style.opacity = "0.2";
+  document.getElementById("Health_Care").style.opacity = "0.2";
+  document.getElementById("Energy").style.opacity = "0.2";
+  document.getElementById("Consumer_Staples").style.opacity = "0.2";
+  document.getElementById("Industrials").style.opacity = "0.2";
+  document.getElementById("Telecommunications").style.opacity = "0.2";
+  document.getElementById("Basic_Materials").style.opacity = "0.2";
+  document.getElementById("Utilities").style.opacity = "1";
+  document.getElementById("title").innerHTML = "<span style='color: #FD5550;'>1</span> out of the top 100 campanies are from Utilities Sector holding a market capitalization of <span style='color: #FD5550;'> 0.50%</span>";
+}
